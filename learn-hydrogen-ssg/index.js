985a4cfd27ef9424ed60d51bcfa1c40c9155a4c4
bojne <bojne.john@gmail.com>
2022-04-20 18:30:16 +0200

    update hydrogen site

--- learn-hydrogen-ssg/index.js
+++ learn-hydrogen-ssg/index.js
@@ -3,10 +3,25 @@ const page = ({ title, data, head }) => `
   <head>
     <title>${title}</title>
     ${head}
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+    <link href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css" rel="stylesheet">
+    <script src="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.js"></script>
+    <style>
+        body { margin: 0; padding: 0; }
+        #map { position: absolute; top: 0; bottom: 0; width: 100%; }
+    </style>
   </head>
   <body>
-    <h2>${data.project}</h2>
-    <p>${data.description}</p>
+    <div id="map"></div>
+    <script>
+    mapboxgl.accessToken = ${data.MapBoxKey};
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [121.597366, 25.105497], // starting position [lng, lat]
+        zoom: 11 // starting zoom
+    });
+    </script>
   <body>
 </html>
 `;
@@ -16,7 +31,9 @@ module.exports = {
   title: "Welcome to Hydrogen",
   data: () => ({
     project: "Hydrogen",
-    description: "Super fast static-site generator",
+    description: "Super fast static-site generator!!",
+    MapBoxKey:
+      "pk.eyJ1IjoiYm9qbmUiLCJhIjoiY2wyN3J1aWk4MDJhbzNlbDNleWJxdWQzbCJ9.DDPlH3SjftRuRlEWRCdlvA",
   }),
   head: ({ data }) => [
     ["meta", { name: "description", content: data.description }],
