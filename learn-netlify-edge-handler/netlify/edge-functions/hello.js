be7daf6a1c6cf3b9a0a1defdf5b4e9c5662e1244
bojne <bojne.john@gmail.com>
2022-04-20 17:48:44 +0200

    add env var to edge function

--- learn-netlify-edge-handler/netlify/edge-functions/hello.js
+++ learn-netlify-edge-handler/netlify/edge-functions/hello.js
@@ -1,1 +1,4 @@
-export default () => new Response("Hello world");
+import "https://deno.land/x/dotenv/load.ts";
+
+export default () =>
+  new Response(`Hello world and Hello ${Deno.env.get("GREETING")}`);
